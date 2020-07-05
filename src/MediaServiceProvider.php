04c5a1e6b955a4ed1053dd511be937cad8ca8e32
drewroberts <github@drewroberts.com>
2020-07-05 14:18:09 -0400

    Create migrations for images & videos

--- src/MediaServiceProvider.php
+++ src/MediaServiceProvider.php
@@ -9,6 +9,8 @@ class MediaServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/media.php' => config_path('media.php'),
@@ -17,12 +19,6 @@ class MediaServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__.'/../resources/views' => base_path('resources/views/vendor/media'),
             ], 'views');
-
-            if (! class_exists('CreatePackageTable')) {
-                $this->publishes([
-                    __DIR__ . '/../database/migrations/create_media_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_media_table.php'),
-                ], 'migrations');
-            }
 
             $this->commands([
                 MediaCommand::class,

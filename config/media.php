2688d08b561fef565170ac1497fba3c4a5c7cde8
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-02-04 16:44:45 -0500

    add classes required in the config file

--- config/media.php
+++ config/media.php
@@ -1,5 +1,14 @@
 <?php
 
 return [
+    /**
+     * Model class references
+     */
+    'model_class' => [
 
+        'image' => \App\Models\Image::class,
+        'tag' => \App\Models\Tag::class,
+        'video' => \App\Models\Video::class
+
+    ]
 ];

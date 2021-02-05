3e0360eec4386c4857adc0ef3212720be1eb5bd7
Drew Roberts <github@drewroberts.com>
2021-02-05 11:46:55 -0500

    Revert config file to use tipoff support config

--- config/media.php
+++ config/media.php
@@ -1,14 +1,5 @@
 <?php
 
 return [
-    /**
-     * Model class references
-     */
-    'model_class' => [
 
-        'image' => \App\Models\Image::class,
-        'tag' => \App\Models\Tag::class,
-        'video' => \App\Models\Video::class
-
-    ]
 ];

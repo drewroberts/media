24daf80d506131e0ef706b3c3bbcd1d9d69418b2
Gustavo Gentil <gustavorgentil@outlook.com>
2021-02-08 13:01:05 -0300

    Move testing database configuration to phpunit.xml

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -20,19 +20,4 @@ class TestCase extends Orchestra
             MediaServiceProvider::class,
         ];
     }
-
-    public function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('database.default', 'sqlite');
-        $app['config']->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
-
-        /*
-        include_once __DIR__.'/../database/migrations/create_media_table.php.stub';
-        (new \CreatePackageTable())->up();
-        */
-    }
 }

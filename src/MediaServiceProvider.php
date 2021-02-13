e830f74137d2fa5bc70da2ac302203c2795a95f7
allibenson <77474875+allibenson@users.noreply.github.com>
2021-02-13 00:30:32 -0500

    Register policies in package service provider #25

--- src/MediaServiceProvider.php
+++ src/MediaServiceProvider.php
@@ -7,6 +7,9 @@ namespace DrewRoberts\Media;
 use Tipoff\Media\Models\Image;
 use Tipoff\Media\Models\Tag;
 use Tipoff\Media\Models\Video;
+use Tipoff\Media\Policies\Image;
+use Tipoff\Media\Policies\Tag;
+use Tipoff\Media\Policies\Video;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -15,6 +18,11 @@ class MediaServiceProvider extends TipoffServiceProvider
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
+            ->hasPolicies([
+                Image::class => ImagePolicy::class,
+                Tag::class => TagPolicy::class,
+                Video::class => VideoPolicy::class,
+            ])
             ->name('media')
             ->hasConfigFile();
     }

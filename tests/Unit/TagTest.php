967f3d62c507f29fdf6fe49adb8acd7f374f5924
Gustavo Gentil <gustavorgentil@outlook.com>
2021-02-15 18:09:42 -0300

    Ensure a tag can be found by its name

--- tests/Unit/TagTest.php
+++ tests/Unit/TagTest.php
@@ -22,6 +22,42 @@ class TagTest extends TestCase
     }
 
     /** @test */
+    public function it_finds_a_tag_by_name()
+    {
+        $name = $this->faker->word;
+        $target = Tag::factory()->create(['name' => $name]);
+
+        $tag = Tag::findFromString($name);
+
+        $this->assertNotNull($tag);
+        $this->assertEquals($tag->id, $target->id);
+
+        $this->assertNull(
+            Tag::findFromString('invalid_name')
+        );
+    }
+
+    /** @test */
+    public function it_finds_a_tag_from_a_string_that_matches_a_certain_type()
+    {
+        $name = $this->faker->word;
+        $type = $this->faker->word;
+
+        Tag::factory()->create([
+            'name' => $name,
+            'type' => $type,
+        ]);
+
+        $this->assertNotNull(
+            Tag::findFromString($name, $type)
+        );
+
+        $this->assertNull(
+            Tag::findFromString($name)
+        );
+    }
+
+    /** @test */
     public function it_lists_all_tag_types_without_repeating_them()
     {
         Tag::factory()->create(['type' => 'type_1']);

f655b3d5dfd8f5d77ca6afa648fafc9710de821a
Gustavo Gentil <gustavorgentil@outlook.com>
2021-02-15 17:26:23 -0300

    Ensure tag has updater and creator set

--- tests/Unit/TagTest.php
+++ tests/Unit/TagTest.php
@@ -3,6 +3,7 @@
 namespace DrewRoberts\Media\Tests\Unit;
 
 use DrewRoberts\Media\Models\Tag;
+use DrewRoberts\Media\Tests\Support\Models\User;
 use DrewRoberts\Media\Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -18,5 +19,32 @@ class TagTest extends TestCase
         $tag = Tag::factory()->create();
 
         $this->assertEquals($tag->path, "/tags/{$tag->slug}");
+    }
+
+    /** @test */
+    public function it_keeps_track_of_who_created_it()
+    {
+        $user = User::factory()->create();
+
+        $this->be($user);
+
+        $tag = Tag::factory()->create();
+
+        $this->assertInstanceOf(User::class, $tag->creator);
+        $this->assertEquals($user->id, $tag->creator->id);
+    }
+
+    /** @test */
+    public function it_keeps_track_of_who_updated_it()
+    {
+        $user = User::factory()->create();
+        $tag = Tag::factory()->make();
+
+        $this->be($user);
+
+        $tag->save();
+
+        $this->assertInstanceOf(User::class, $tag->updater);
+        $this->assertEquals($user->id, $tag->updater->id);
     }
 }

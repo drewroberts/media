dbd2deecad50dbe178931c0a272c413af3212074
Drew Roberts <github@drewroberts.com>
2021-02-22 10:23:44 -0500

    Seed media permissions with migration

---
+++ database/migrations/2021_02_12_120000_add_media_permissions.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Spatie\Permission\Contracts\Permission;
+use Spatie\Permission\PermissionRegistrar;
+
+class AddBlogPermissions extends Migration
+{
+    public function up()
+    {
+        if (app()->has(Permission::class)) {
+            app(PermissionRegistrar::class)->forgetCachedPermissions();
+
+            foreach ([
+                         'view images',
+                         'create images',
+                         'update images',
+                         'view tags',
+                         'create tags',
+                         'update tags',
+                         'delete tags',
+                         'view videos',
+                         'create videos',
+                         'update videos',
+                     ] as $name) {
+                app(Permission::class)::findOrCreate($name, null);
+            };
+        }
+    }
+}

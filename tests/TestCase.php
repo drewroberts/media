21d66b901039e54c3406642ffe99f8bf02c880ec
drewroberts <drewroberts@users.noreply.github.com>
2021-03-12 03:28:32 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 
 namespace DrewRoberts\Media\Tests;
 
+use DrewRoberts\Media\MediaServiceProvider;
+use DrewRoberts\Media\Tests\Support\Providers\NovaPackageServiceProvider;
 use Laravel\Nova\NovaCoreServiceProvider;
 use Spatie\Permission\PermissionServiceProvider;
 use Tipoff\Authorization\AuthorizationServiceProvider;
-use DrewRoberts\Media\MediaServiceProvider;
-use DrewRoberts\Media\Tests\Support\Providers\NovaPackageServiceProvider;
 use Tipoff\Support\SupportServiceProvider;
 use Tipoff\TestSupport\BaseTestCase;
 

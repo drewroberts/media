877c33c6fb0fa7fd49b00cd6822c692eaf059d7d
drewroberts <drewroberts@users.noreply.github.com>
2021-03-17 13:39:32 +0000

    Fix styling

--- src/Nova/Video.php
+++ src/Nova/Video.php
@@ -4,7 +4,6 @@ namespace DrewRoberts\Media\Nova;
 
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
-use Laravel\Nova\Fields\DateTime;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Text;

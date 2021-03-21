19b3d7f6c361b71db9dc1913813a6d2bd66e3fe2
Hunter Montell <64396917+huntermontell@users.noreply.github.com>
2021-03-21 15:28:56 -0500

    Remove import of deleted class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace DrewRoberts\Media\Tests;
 
 use DrewRoberts\Media\MediaServiceProvider;
-use DrewRoberts\Media\Tests\Support\Providers\NovaPackageServiceProvider;
 use Laravel\Nova\NovaCoreServiceProvider;
 use Silvanite\NovaFieldCloudinary\Providers\PackageServiceProvider;
 use Spatie\Permission\PermissionServiceProvider;
 use Tipoff\Authorization\AuthorizationServiceProvider;
 use Tipoff\Support\SupportServiceProvider;
 use Tipoff\TestSupport\BaseTestCase;
+use Tipoff\TestSupport\Providers\NovaPackageServiceProvider;
 
 class TestCase extends BaseTestCase
 {

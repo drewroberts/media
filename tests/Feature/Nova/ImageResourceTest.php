3efd6646a19975dcb697794567864e4e800d553e
Drew Roberts <github@drewroberts.com>
2021-03-24 22:03:14 -0400

    Update image resource test #12

--- tests/Feature/Nova/ImageResourceTest.php
+++ tests/Feature/Nova/ImageResourceTest.php
@@ -7,19 +7,16 @@ namespace DrewRoberts\Media\Tests\Feature\Nova;
 use DrewRoberts\Media\Models\Image;
 use DrewRoberts\Media\Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Config;
+use Tipoff\Authorization\Models\User;
 
 class ImageResourceTest extends TestCase
 {
-    //use DatabaseTransactions;
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
     /** @test */
     public function index()
     {
-        Config::set('app.key', 'base64:CA0WFs+ECA4gq/G95GpRwEaYsoNdUF0cAziYkc83ISE=');
-
         Config::set('filesystems.disks.cloudinary', [
             'driver' => 'cloudinary',
             'api_key' => env('CLOUDINARY_API_KEY', 'nonsense'),
@@ -27,12 +24,34 @@ class ImageResourceTest extends TestCase
             'cloud_name' => env('CLOUDINARY_CLOUD_NAME', 'nonsense'),
         ]);
         
-        Image::factory()->count(1)->create();
+        Image::factory()->count(4)->create();
 
-        $this->actingAs(self::createPermissionedUser('view images', true));
+        $this->actingAs(User::factory()->create()->assignRole('Admin'));
 
-        $response = $this->getJson('nova-api/images')->assertOk();
+        $response = $this->getJson('nova-api/images')
+            ->assertOk();
 
-        $this->assertCount(1, $response->json('resources'));
+        $this->assertCount(4, $response->json('resources'));
+    }
+
+    /** @test */
+    public function show()
+    {
+        Config::set('filesystems.disks.cloudinary', [
+            'driver' => 'cloudinary',
+            'api_key' => env('CLOUDINARY_API_KEY', 'nonsense'),
+            'api_secret' => env('CLOUDINARY_API_SECRET', 'nonsense'),
+            'cloud_name' => env('CLOUDINARY_CLOUD_NAME', 'nonsense'),
+        ]);
+        
+        $user = User::factory()->create();
+        $image = Image::factory()->create();
+
+        $this->actingAs(User::factory()->create()->assignRole('Admin'));
+
+        $response = $this->getJson("nova-api/images/{$image->id}")
+            ->assertOk();
+
+        $this->assertEquals($image->id, $response->json('resource.id.value'));
     }
 }

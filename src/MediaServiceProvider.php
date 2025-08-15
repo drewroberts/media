84923ef0504221196887276222c1c064ee6bad24
Drew Roberts <github@drewroberts.com>
2025-08-15 17:50:48 -0400

    Update service provider

--- src/MediaServiceProvider.php
+++ src/MediaServiceProvider.php
@@ -18,8 +18,12 @@ class MediaServiceProvider extends PackageServiceProvider
         $package
             ->name('media')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_media_table')
-            ->hasCommand(MediaCommand::class);
+            ->hasMigrations([
+                '2015_05_20_100000_create_images_table',
+                '2015_05_20_110000_create_videos_table',
+                '2015_05_20_120000_create_tags_table',
+                '2015_05_20_121000_create_taggables_table',
+            ])
+            ->runsMigrations();
     }
 }

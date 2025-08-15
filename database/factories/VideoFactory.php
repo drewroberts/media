747c8c706e46cd0cfa9637bdf5fb6494d800aac4
Drew Roberts <github@drewroberts.com>
2025-08-15 17:34:09 -0400

    Restore db factories

---
+++ database/factories/VideoFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DrewRoberts\Media\Database\Factories;
+
+use App\Models\User;
+use DrewRoberts\Media\Models\Image;
+use DrewRoberts\Media\Models\Video;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class VideoFactory extends Factory
+{
+    protected $model = Video::class;
+
+    public function definition()
+    {
+        $word = $this->faker->unique()->word;
+
+        return [
+            'identifier'   => $this->faker->unique->isbn10,
+            'source'       => $this->faker->randomElement(['youtube', 'vimeo', 'other']),
+            'name'         => $this->faker->unique->sentence,
+            'title'        => $word,
+            'description'  => $this->faker->sentences(1, true),
+            'image_id'     => randomOrCreate(Image::class),
+            'creator_id'    => randomOrCreate(User::class),
+            'updater_id'    => randomOrCreate(User::class),
+        ];
+    }
+}

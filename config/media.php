8d1980760ebb9ee3e9ad4eb97344035bff32a9c1
Drew Roberts <github@drewroberts.com>
2025-08-15 17:34:39 -0400

    Restore config

--- config/media.php
+++ config/media.php
@@ -2,5 +2,7 @@
 
 // config for DrewRoberts/Media
 return [
-
+    'cloudinary_cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+    'cloudinary_api_key' => env('CLOUDINARY_API_KEY'),
+    'cloudinary_api_secret' => env('CLOUDINARY_API_SECRET'),
 ];

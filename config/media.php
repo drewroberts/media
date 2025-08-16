1c4801774c1494d53c09f822be4a9478ebb73a7d
Drew Roberts <github@drewroberts.com>
2025-08-16 12:55:09 -0400

    Update cloudinary env variables in config

--- config/media.php
+++ config/media.php
@@ -2,8 +2,38 @@
 
 // config for DrewRoberts/Media
 return [
+
     // Use env() with default null values (allowed inside config files)
-    'cloudinary_cloud_name' => env('CLOUDINARY_CLOUD_NAME', null),
-    'cloudinary_api_key' => env('CLOUDINARY_API_KEY', null),
-    'cloudinary_api_secret' => env('CLOUDINARY_API_SECRET', null),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cloudinary Configuration
+    |--------------------------------------------------------------------------
+    |
+    | An HTTP or HTTPS URL to notify your application (a webhook) when the process of uploads, deletes, and any API
+    | that accepts notification_url has completed.
+    |
+    | @todo Create a notification url within this media package to handle saving response to database..
+    |
+    */
+    env('CLOUDINARY_NOTIFICATION_URL', null),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cloudinary Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your Cloudinary settings. Cloudinary is a cloud hosted
+    | media management service for all file uploads, storage, delivery and transformation needs.
+    |
+    |
+    */
+    env('CLOUDINARY_URL', null),
+    env('CLOUDINARY_KEY', null),
+    env('CLOUDINARY_SECRET', null),
+    env('CLOUDINARY_CLOUD_NAME', null),
+    env('CLOUDINARY_UPLOAD_PRESET', null),
+    env('CLOUDINARY_UPLOAD_ROUTE', null),
+    env('CLOUDINARY_UPLOAD_ACTION', null),
+
 ];

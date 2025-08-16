09103a7ecbe09017d8cdc001ba83cabe435ee3e5
Drew Roberts <github@drewroberts.com>
2025-08-15 20:10:19 -0400

    Remove table test migrations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -35,14 +35,6 @@ class TestCase extends Orchestra
             'prefix' => '',
         ]);
 
-        // Load package migrations
-        $migration = include __DIR__.'/../database/migrations/2015_05_20_100000_create_images_table.php';
-        $migration->up();
-
-        // If you have a tags migration
-        $tagsMigration = include __DIR__.'/../database/migrations/2015_05_20_200000_create_tags_table.php';
-        $tagsMigration->up();
-
         // Create users table for testing
         $app['db']->connection()->getSchemaBuilder()->create('users', function ($table) {
             $table->increments('id');
